f7770d0e499e698d87275aa232a5e1fb2375515a
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-27 12:36:59 -0500

    change title

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <head>
 
     <!-- HTML Required Tags -->
-    <title>Business Letter Composer</title>
+    <title>Business Letter PDF Composer</title>
     <meta charset="utf-8"/>
 
     <!-- Adobe Fonts -->
@@ -30,7 +30,7 @@
     <main>
 
         <!-- Page Title Header -->
-        <h1 id='title'><a href=''>Business Letter Generator</a></h1>
+        <h1 id='title'><a href=''>Business Letter PDF Composer</a></h1>
 
 
         <!-- The User Interface -->
@@ -169,7 +169,7 @@
 
 
             <!-- Submit Button / Make the PDF -->
-            <input type='submit' name='submit' value='Make PDF'/>
+            <input type='submit' name='submit' value='Compose PDF'/>
 
         </form>
 
