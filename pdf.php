c359ff957e2551b7cfc84e7f37af1189a8fafc3d
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-27 03:00:25 -0500

    test change

--- pdf.php
+++ pdf.php
@@ -3,4 +3,24 @@
 #   A SCRIPT TO GENERATE A BUSINESS LETTER PDF
 ################################################################################
 
-echo "test";
+// Include the DOMPDF Class
+require_once './library/dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+
+    // Initiate DOMPDF
+    $dompdf = new Dompdf();
+
+    // Set Font
+    $dompdf->set_option('defaultFont', 'Times');
+
+    // Set HTML
+    $dompdf->loadHtml("hello world");
+
+    // Set paper size and orientation
+    $dompdf->setPaper('A4', 'portrait');
+
+    // Render the HTML as PDF
+    $dompdf->render();
+
+    // Print Out PDF
+    $dompdf->stream("xxx.pdf", array("Attachment" => false));
