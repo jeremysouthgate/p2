6359a7c575be6f70ab3fa4261d1df78378906ba6
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-27 10:23:25 -0500

    testing dompdf on live

--- test.php
+++ test.php
@@ -3,21 +3,4 @@
 #   A SCRIPT TO GENERATE A BUSINESS LETTER PDF
 ################################################################################
 
-// include autoloader
-require_once $_SERVER['DOCUMENT_ROOT']."/library/dompdf/autoload.inc.php";
-
-// reference the Dompdf namespace
-use Dompdf\Dompdf;
-
-// instantiate and use the dompdf class
-$dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
-
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
-
-// Render the HTML as PDF
-$dompdf->render();
-
-// Output the generated PDF to Browser
-$dompdf->stream();
+echo phpinfo();
