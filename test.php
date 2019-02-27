da652e623947683c96f6ecc4724c480d63ea0bdd
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-27 10:09:06 -0500

    testing dompdf on live

--- test.php
+++ test.php
@@ -4,27 +4,4 @@
 ################################################################################
 
 // Include the DOMPDF Class
-require_once './library/dompdf/lib/html5lib/Parser.php';
-require_once './library/dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
-require_once './library/dompdf/lib/php-svg-lib/src/autoload.php';
-require_once './library/dompdf/src/Autoloader.php';
-Dompdf\Autoloader::register();
-use Dompdf\Dompdf;
-
-// Initiate DOMPDF
-$dompdf = new Dompdf();
-
-// Set Font
-$dompdf->set_option('defaultFont', 'Times');
-
-// Set HTML
-$dompdf->loadHtml("Good Morning.");
-
-// Set paper size and orientation
-$dompdf->setPaper('A4', 'portrait');
-
-// Render the HTML as PDF
-$dompdf->render();
-
-// Print Out PDF
-$dompdf->stream("test.pdf", array("Attachment" => false));
+echo "test123";
